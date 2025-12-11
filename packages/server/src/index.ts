35e3db556e9b3299092bcf881957884892b582fe
Anthony Lopes <144701824+AnthxnyL@users.noreply.github.com>
2025-12-11 16:52:34 +0100

    Feat: add smoke test (#22)
    * feat: add push test
    
    * feat: add build docker environment production
    
    * feat: add vercel token
    
    * feat: add vercel token
    
    * feat: add smoke test
    
    * fix: environnement cd
    
    * feat: update path
    
    ---------
    
    Co-authored-by: Anthony <anthonylopes037@gmail.com>

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,6 +2,13 @@ import { app } from "./app.js";
 
 const PORT = process.env.PORT || 3010;
 
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 'ok',
+    timestamp: new Date().toISOString(),
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
